978154357f3cd3efdf71df9dd81dd06afc0786d4
Darbyblinko <jacobcrombie@live.com>
2021-08-24 18:53:27 -0600

    added food menu three

---
+++ src/views/FoodMenuThree.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="food-menu-three"></div>
+</template>
+
+
+<script>
+export default {
+  name: "food-menu-three",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {},
+};
+</script>
+
+
+<style scoped>
+</style>
